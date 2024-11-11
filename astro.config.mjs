1118dc5fcff2b57817c433d6b2b520bee8ffa411
Dallas James <iam@dallastjames.dev>
2024-11-11 14:28:39 -0600

    disable trailing slash on urls

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import starlight from "@astrojs/starlight";
 export default defineConfig({
   site: "https://www.rpgsessions.com",
   base: "/discord-bot-docs",
+  trailingSlash: "never",
   integrations: [
     starlight({
       title: "RPG Sessions Discord Bot",
